b8d32a1805c26422293efa116098c5326571d32a
MiltonP <milton206@gmail.com>
2023-06-19 23:25:23 -0400

    mongo conection

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true) //Linea de codigo soluciona bug de moongose 7
 //mongoose.connect('mongodb://localhost:27017/crudmern') // conexion sistema local mongoDB
-const URL_MONGOCONNECTION = mongoose.connect(process.env.URL_MONGOCONNECTION,{ useNewUrlParser: true,
+ mongoose.connect(process.env.URL_MONGOCONNECTION,{ useNewUrlParser: true,
   useUnifiedTopology: true,});
 
 
@@ -12,7 +12,7 @@ const objetobd= mongoose.connection
 objetobd.on('connected',()=>{console.log('conexion correcta a MongoDB')})
 objetobd.on('error',()=>{console.log('error en la conexion a MongoDB')})
 
-module.exports= {mongoose,URL_MONGOCONNECTION}
+module.exports= mongoose
 
 
 require('dotenv').config()
